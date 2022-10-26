e20aa39d1aa7f4fb3d06477d01231535513578c7
GineTik <densheva18072003@gmail.com>
2022-10-26 13:58:59 +0300

    added implementations of repositories

---
+++ MusicShop.DataAccess/Repository/Implementations/MusicRepository.cs
@@ -0,0 +1,13 @@
+﻿using MusicShop.Core.Entities;
+using MusicShop.DataAccess.EF;
+using MusicShop.DataAccess.Repository.Interfaces;
+
+namespace MusicShop.DataAccess.Repository.Implementations
+{
+    public class MusicRepository : Repository<Music>, IMusicRepository
+    {
+        public MusicRepository(DataContext dataContext) : base(dataContext)
+        {
+        }
+    }
+}
--- MusicShop.DataAccess/Repository/Implementations/Repository.cs
+++ MusicShop.DataAccess/Repository/Implementations/Repository.cs
@@ -1,54 +1,50 @@
 ﻿using MusicShop.Core.Entities;
 using MusicShop.DataAccess.EF;
 using MusicShop.DataAccess.Repository.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MusicShop.DataAccess.Repository.Implementations
 {
-    public class Repository<TEntity, TKey> : IRepository<TEntity>
-        where TEntity : IBaseEntity
-        where TKey : IEquatable<TKey>
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IBaseEntity
     {
         private readonly DataContext _db;
 
         public Repository(DataContext dataContext)
         {
-            this._db = dataContext;
+            _db = dataContext;
         }
 
         public void Add(TEntity entity)
         {
-            this._db.Set<TEntity>().Add(entity);
+            _db.Set<TEntity>().Add(entity);
         }
 
         public void Dispose()
         {
-            this._db.Dispose();
+            _db.Dispose();
         }
 
         public IEnumerable<TEntity> GetAll()
         {
-            return this._db.Set<TEntity>();
+            return _db.Set<TEntity>();
         }
 
         public TEntity GetById(int id)
         {
-            return this._db.Set<TEntity>().FirstOrDefault(e => e.Id == id);
+            return _db.Set<TEntity>().FirstOrDefault(e => e.Id == id);
         }
 
         public void Remove(int id)
         {
             // найти как работает удаление по модели, мб можно изменить сравнение
-            this._db.Set<TEntity>().Remove(GetById(id));
+            _db.Set<TEntity>().Remove(GetById(id));
         }
 
         public void Update(TEntity entity)
         {
-            this._db.Set<TEntity>().Update(entity);
+            _db.Set<TEntity>().Update(entity);
         }
     }
 }
