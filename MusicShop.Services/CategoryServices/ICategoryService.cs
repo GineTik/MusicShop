6614fe7d083b297ea2a7706779d56fe73dd39e80
Artur358 <itgamevenpc@gmail.com>
2022-11-23 17:20:25 +0200

    create base controller

---
+++ MusicShop.Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using MusicShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicShop.Services.CategoryServices
+{
+    public interface ICategoryServices
+    {
+        Category GetById(int categoryId);
+        IEnumerable<Category> GetAll();
+        Category Create(Category category);
+        bool Delete(int categoryId);
+        Category Edit(Category category);
+    }
+}
---
+++ MusicShop/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicShop.WebHost.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
